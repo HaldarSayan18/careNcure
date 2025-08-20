1ef73039304cd236dac5dc4e2f14f35496d8b363
SayanH-BIT <sayan.haldar.brainium@gmail.com>
2025-08-20 23:50:41 +0530

    admin header design done

---
+++ frontend/src/layout/AdminHeader.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import '../App.css';
+import logo from '../assets/logo.png';
+import { Link, Outlet, useLocation } from 'react-router-dom';
+
+const Header = () => {
+    const location = useLocation();
+    const getNavItem = (path) => {
+        return location.pathname === path ? 'active-navbar-item' : '';
+    };
+    return (
+        <div className='root-container'>
+            <div className='admin-navbar-container'>
+                <div className='admin-logo-section'>
+                    <img src={logo} alt='logo' className='logo' />
+                    <span>Namaste Admin!</span>
+                </div>
+                <div className='navbar-items'>
+                    <Link to="/" className={getNavItem('/')} style={{ textDecoration: "none", color: "inherit" }}><span>Home</span></Link>
+                    <Link to="/doctors" className={getNavItem('/doctors')} style={{ textDecoration: "none", color: "inherit" }}><span>Add Speciality</span></Link>
+                    <Link to="/doctors" className={getNavItem('/doctors')} style={{ textDecoration: "none", color: "inherit" }}><span>Add Doctor</span></Link>
+                    {/* <span>Doctor</span> */}
+                    <Link to="/appointment" className={getNavItem('/appointment')} style={{ textDecoration: "none", color: "inherit" }}><span>Appointment Control</span></Link>
+                    <Link to="/bed" className={getNavItem('/bed')} style={{ textDecoration: "none", color: "inherit" }}><span>Bed</span></Link>
+                    <Link to="/about-us" className={getNavItem('/about-us')} style={{ textDecoration: "none", color: "inherit" }}><span>About Us</span></Link>
+                </div>
+            </div>
+            <div className='other-containers'>
+                <Outlet />
+            </div>
+        </div>
+    )
+}
+
+export default Header

e8f21f32a7fa09226d8fcef5c488721350f1a856
Tridip Pramanik <154905818+gitTridip@users.noreply.github.com>
2025-08-22 07:55:56 +0000

    patient implemented

--- backend/src/main/java/com/careNcure/backend/authentication/UserCredentials.java
+++ backend/src/main/java/com/careNcure/backend/authentication/UserCredentials.java
@@ -5,7 +5,37 @@ public class UserCredentials{
     @Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+    @Column(unique=true)
     private String email;
+    @Column(unique=true,nullable=false)
     private long mobile;
+    @Column(nullable=false)
     private String password;
+    @Column(nullable=false)
+    private Role role;
+    
+    public String getEmail(){
+        return this.email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public long getMobile(){
+        return this.mobile;
+    }
+    public void setMobile(long mobile){
+        this.mobile=mobile;
+    }
+    public String getPassword(){
+        return this.password;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+    public Role getRole(){
+        return this.role;
+    }
+    public void setRole(Role role){
+        this.role=role;
+    }
 }
